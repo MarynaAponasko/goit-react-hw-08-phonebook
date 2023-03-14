070a41ca6b8ad65e1b5ec2e84408bd1ec923617a
MarynaAponasko <aponasko.mari@gmail.com>
2023-03-14 18:43:46 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+import PrivateRoute from 'components/modules/PrivateRoute.jsx/PrivateRoute';
+import PublicRoute from 'components/modules/PublicRoute/PublicRoute';
+
 import Layout from 'components/Layout/Layout';
-import HomePage from 'components/Pages/HomePage/HomePage';
-import LogInPage from 'components/Pages/LogInPage/LogInPage';
-import SignUpPage from 'components/Pages/SignUpPage/SignUpPage';
-import Phonebook from 'components/Pages/Phonebook/Phonebook';
+import Loader from 'components/modules/Loader/Loader';
+
+const HomePage = lazy(() => import('./components/Pages/HomePage/HomePage'));
+const LogInPage = lazy(() => import('./components/Pages/LogInPage/LogInPage'));
+const SignUpPage = lazy(() =>
+  import('./components/Pages/SignUpPage/SignUpPage')
+);
+const Phonebook = lazy(() => import('./components/Pages/Phonebook/Phonebook'));
+// import HomePage from 'components/Pages/HomePage/HomePage';
+// import LogInPage from 'components/Pages/LogInPage/LogInPage';
+// import SignUpPage from 'components/Pages/SignUpPage/SignUpPage';
+// import Phonebook from 'components/Pages/Phonebook/Phonebook';
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="phonebook" element={<Phonebook />} />
-          <Route path="login" element={<LogInPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route element={<PublicRoute />}>
+              <Route index element={<HomePage />} />
+              <Route path="login" element={<LogInPage />} />
+              <Route path="signup" element={<SignUpPage />} />
+            </Route>
+
+            <Route element={<PrivateRoute />}>
+              <Route path="phonebook" element={<Phonebook />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { App } from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import AuthLayout from 'components/modules/AuthLayote/AuthLayote';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <AuthLayout>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthLayout>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 );
